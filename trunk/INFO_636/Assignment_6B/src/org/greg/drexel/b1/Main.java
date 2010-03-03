ff646f7a13951d2a5d636c4892e4cb8bedcfc0da
greg.vannoni@gmail.com <greg.vannoni@gmail.com@75f3a394-f9b6-11de-9e29-7d1cd92887ef>
2010-03-03 04:52:55 +0000

    branching to make assignment 6B

---
+++ trunk/INFO_636/Assignment_6B/src/org/greg/drexel/b1/Main.java
@@ -0,0 +1,237 @@
+package org.greg.drexel.b1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.greg.drexel.b1.gui.MainFrame;
+import org.greg.drexel.b1.io.MyFileReader;
+import org.greg.drexel.b1.io.MyFileWriter;
+import org.greg.drexel.b1.types.FileModeType;
+
+/**
+ * @author Greg Vannoni
+ * @class INFO 636
+ *
+ * Purpose: This is the main class that does all of the work.
+ *  - Initialize the GUI
+ *  - Ask user for new file name or existing file name
+ *  - Ask for user input for READ. WRITE  or MODIFY mode
+ *    - If write mode is selected, it will NOT append to a file, if it exists
+ *  - If READ mode: Display the contents of the file in a JList
+ *  - If WRITE mode: 
+ *    - Ask the user for how many Real numbers they plan to input
+ *    - Get the input from the user
+ *    - Write this input to a file
+ *  - If MODIFY mode:
+ *    - Ask the user if they wish to Replace, Insert, Delete, Accept All, or Accept
+ *    
+ *  - Added feature of File > Save As... capability per Program 2B spec.
+ *  - Added filename checking (validity and per read/write/modify mode) per Program 4B spec
+ *  - Added 2-D array support per Program 5B spec
+ *    
+ * @version 5.0
+ * 
+ * Notes:
+ * - MAKE SURE to update ENTRIES_PER_ROW below for the number of columns to
+ *  want to support
+ * 
+ */
+public class Main {
+
+    // This is the K value alluded to in assignment 5B
+    //  Users will enter in ENTRIES_PER_ROW for each row
+    public static final int ENTRIES_PER_ROW = 5;
+    
+    
+    
+    /**
+     * Method: getFileLocation<br/>
+     * Displays the file chooser for a user to select or name their own file
+     * 
+     * @param f - The MainFrame object
+     * @return the full file path of the file the user has selected to create
+     */
+    public static String getFileLocation( MainFrame f )
+    {
+        String fileLocation = null;
+        
+        // Ask the user for a file location
+        fileLocation   = f.getFileLocation("Create or Select File", ".txt files", "txt");
+        
+        return fileLocation;
+    }
+    
+    
+	/***************************************************************************************************
+	 * MAIN METHOD
+	 */
+	public static void main(String[] args) 
+	{
+	    // Setup a Swing Frame
+		MainFrame f = new MainFrame();
+		
+		String fileLocation = getFileLocation( f );
+		
+		// Ask the user for READ or WRITE mode for that file
+		FileModeType mode     = f.getReadWriteModifyMode();
+		 
+		
+		// Handle the READ case
+		if( mode == FileModeType.READ )
+		{
+		    try
+            {
+		        // Verify that the file about to be read from exists
+		        boolean exit = false;
+		        while( !exit )
+		        {
+		           File fileToRead = new File( fileLocation );
+		           if( !fileToRead.exists() )
+		           {
+		               f.displayWarning("The file you selected does not exist for reading!", "File does not exist!");
+		               fileLocation = getFileLocation( f );
+		           }
+		           else
+		           {
+		               exit = true;
+		           }
+		        }
+		        
+		        
+		        // Start up a file reader and get the contents as an ArrayList<String>
+		        // ArrayList<String> was chosen for versatility later, there is no need to require a Real number in this file.
+		        MyFileReader reader = new MyFileReader( fileLocation );
+		        ArrayList<ArrayList<String>> fileContents = new ArrayList<ArrayList<String>>();
+		        fileContents = reader.getFileContents();
+		        
+		        // Don't add to the JList if there is nothing in the file
+		        if( fileContents.size() > 0 )
+		        {
+		            for( ArrayList<String> arr : fileContents )
+		            {
+		                if( arr.size() != ENTRIES_PER_ROW )
+		                {
+		                    f.displayError("The K in the application does not match the number of columns in the data!\nK = " + ENTRIES_PER_ROW + ", Number of columns = " + arr.size(), "Column number mismatch!");
+		                    System.exit(-20);
+		                }
+		            }
+		            
+		            f.displayArrayList( fileContents );
+		        }
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            
+            // Show the window with the file contents
+            f.initializeAndDisplay();
+		}
+		
+		// Handle WRITE mode
+		else if( mode == FileModeType.WRITE )
+		{
+		    // Verify that the file about to be written to exists
+            boolean exit = false;
+            while( !exit )
+            {
+               File fileToRead = new File( fileLocation );
+               if( fileToRead.exists() )
+               {
+                   f.displayWarning("The file you selected already exists, will not write to existing file!", "File already exists!");
+                   fileLocation = getFileLocation( f );
+               }
+               else
+               {
+                   exit = true;
+               }
+            }
+            
+		    // Ask the user for how many rows of Real numbers they plan on inputting
+		    Integer quantityOfNumbersToInput = f.getQuantityOfNumbers();
+		    
+		    ArrayList<ArrayList<String>> inputNumbers = new ArrayList<ArrayList<String>>();
+		    
+		    // For as many ENTRIES_PER_ROW ask for input for these number of columns of data
+		    for( int i=0; i<quantityOfNumbersToInput; i++ )
+		    {
+		        ArrayList<String> input = f.getNumbersInput( i+1, ENTRIES_PER_ROW );
+		        inputNumbers.add( input );
+		    }
+		    
+		    
+		    try
+            {
+		        // Write the contents of their input (all Doubles/Real numbers) to the file they specified
+                MyFileWriter writer = new MyFileWriter( fileLocation );
+                writer.setFileContentsWithArray( inputNumbers );
+                writer.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            
+            if( inputNumbers.size() > 0 )
+	        {
+	            f.displayArrayList( inputNumbers );
+	        }
+            
+            // Show the window with the file contents
+            f.initializeAndDisplay();
+            
+		}
+		else if( mode == FileModeType.MODIFY )
+		{
+			try
+            {
+			    // Verify that the file about to be modified from exists
+                boolean exit = false;
+                while( !exit )
+                {
+                   File fileToRead = new File( fileLocation );
+                   if( !fileToRead.exists() )
+                   {
+                       f.displayWarning("The file you selected does not exist for modification!", "File does not exist!");
+                       fileLocation = getFileLocation( f );
+                   }
+                   else
+                   {
+                       exit = true;
+                   }
+                }
+			    
+		        // Start up a file reader and get the contents as an ArrayList<String>
+		        // ArrayList<String> was chosen for versatility later, there is no need to require a Real number in this file.
+		        MyFileReader reader = new MyFileReader( fileLocation );
+		        ArrayList<ArrayList<String>> fileContents = new ArrayList<ArrayList<String>>();
+		        fileContents = reader.getFileContents();
+		        
+		        for( ArrayList<String> arr : fileContents )
+                {
+                    if( arr.size() != ENTRIES_PER_ROW )
+                    {
+                        f.displayError("The K in the application does not match the number of columns in the data!\nK = " + ENTRIES_PER_ROW + ", Number of columns = " + arr.size(), "Column number mismatch!");
+                        System.exit(-20);
+                    }
+                }
+		        
+		        // Because this is a modify operation, prompt the user for action
+		        f.initializeAndDisplayWithPrompt( fileContents );
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+			
+		}
+		else
+		{
+		    System.err.println("Unsupported mode! Exiting!");
+		    System.exit(-1);
+		}
+
+	} // end main
+
+} // end class
---
+++ trunk/INFO_636/Assignment_6B/src/org/greg/drexel/b1/gui/MainFrame.java
@@ -0,0 +1,553 @@
+package org.greg.drexel.b1.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.greg.drexel.b1.Main;
+import org.greg.drexel.b1.io.MyFileWriter;
+import org.greg.drexel.b1.types.FileModeType;
+import org.greg.drexel.b1.types.FileModifyType;
+
+/**
+ * @author Greg Vannoni
+ * @class INFO 636
+ * Purpose: Display dialog and input boxes to the user in order to:
+ *  1. Obtain input to write (Real numbers) into a file
+ *  2. Display the contents of a file (with Real numbers)
+ *  3. New in 2B: Ask user if they want to Modify, Insert, Replace, Accept or Accept each line in a file
+ *  4. New in 4B: Validate the filename is a valid windows filename
+ *  5. New in 5B: Support for 2-D arrays of data instead of 1 column, 1 row like before
+ *  
+ *  @version 6.0
+ *  Notes:
+ *  
+ * 
+ */
+public class MainFrame extends JFrame
+{
+
+    // Configurable variables
+    private final String WINDOW_TITLE = "INFO 636 - Program 5B - Number Retreiver/Modifier";
+    private final Dimension DEFAULT_WINDOW_SIZE = new Dimension(500,500);
+    
+    
+    private static final long serialVersionUID = 1L;
+    private DefaultListModel jListModel = null;
+    
+    /**
+     * Method: MainFrame<br/>
+     * Main constructor for initialization
+     * 
+     */
+    public MainFrame()
+    {
+        jListModel = new DefaultListModel();
+    }
+
+    
+    /**
+     * Method: displayError<br/>
+     * Displays an error dialog box to the user.
+     * 
+     * @param message - The message to display to the user
+     * @param title - The title of the dialog box
+     */
+    public void displayError( String message, String title )
+    {
+        JOptionPane.showMessageDialog(this, message,title, JOptionPane.ERROR_MESSAGE );
+    }
+  
+    
+    /**
+     * Method: displayWarning<br/>
+     * Displays a warning dialog box to the user.
+     *
+     * @param message - The message to display to the user
+     * @param title - The title of the dialog box
+     */
+    public void displayWarning( String message, String title )
+    {
+        Object[] options = { "Ok", "Terminate Program" };
+        int selection = JOptionPane.showOptionDialog(this, message, title, JOptionPane.OK_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0] );
+        
+        // If the user wants to quit, quit.
+        if( selection == 1 )
+        {
+           System.exit(-6);
+        }
+    }
+    
+    
+    /**
+     * Method: displayArrayList<br/>
+     * Display an ArrayList of Strings in the JList
+     *
+     * @param the ArrayList<ArrayList<String>> to display in the JList
+     */
+    public void displayArrayList( ArrayList<ArrayList<String>> arrayList )
+    {
+        
+        for( ArrayList<String> arr : arrayList )
+        {
+            String line = "";
+            for( Object o : arr )
+            {
+                line = line + "  " + o;
+            }
+            line = line.trim();
+            displaySingleRow( line );
+        }
+    }
+    
+    /**
+     * Method: displayArrayListSingleRow<br/>
+     * Display an arraylist as a single row on the JList
+     *
+     * @param the ArrayList<String> to display in the JList
+     */
+    public void displayArrayListSingleRow( ArrayList<String> arrayList )
+    {
+        String line = "";
+        for( String s : arrayList )
+        {
+            line = line + "  " + s;
+        }
+        line = line.trim();
+        displaySingleRow( line );
+    }
+    
+    
+    /**
+     * Method: getJListAsArrayList<br/>
+     * Convert the JList to an ArrayList<String>
+     *
+     * @return ArrayList<String> with JList contents
+     */
+    public ArrayList<String> getJListAsArrayList( )
+    {
+    	ArrayList<String> result = new ArrayList<String>();
+    	
+    	for( Object o : jListModel.toArray() )
+    	{
+    		result.add( o.toString() );
+    	}
+    	
+    	return result;
+    }
+    
+    
+    /**
+     * Method: displaySingleRow<br/>
+     * Display an ArrayList of Strings in the JList
+     *
+     * @param arrayList<String> - the ArrayList to display in the JList
+     */
+    public void displaySingleRow( Object strToDisplay )
+    {
+    	jListModel.addElement( strToDisplay );
+    }
+    
+    
+    /**
+     * Method: removeSingleRow<br/>
+     * Remove a string (one row) from the JList
+     *
+     * @param strToRemove - The string or ArrayList<String> to remove from the JList
+     */
+    public void removeSingleRow( Object strToRemove )
+    {
+        if( strToRemove instanceof ArrayList<?> )
+        {
+            String line = "";
+            for( String s : (ArrayList<String>)strToRemove )
+            {
+                line = line + "  " + s;
+            }
+            line = line.trim();
+            jListModel.removeElement( line );
+        }
+        else
+        {
+            jListModel.removeElement( strToRemove );
+        }
+    }
+    
+    
+    /**
+     * Method: getJListPane<br/>
+     * Get the JList (initialized with the jListModel) for the GUI
+     *
+     * @return JList
+     */
+    private JList getJListPane()
+    {
+        
+        JList list = new JList( jListModel );
+        return list;
+    }
+    
+    
+    /**
+     * Method: getModifyAction<br/>
+     * Ask the user if they want to:
+     *  - Accept - Print a single line of the file
+     *  - Accept All - Print the remainder of the lines in the file
+     *  - Delete - Remove the current row from the JList
+     *  - Insert - Insert a row after the row currently displayed
+     *  - Replace - Replace the current line with with user input
+     *
+     * @return the FileModifyType of modification the user wants to perform
+     */
+    public FileModifyType getModifyAction()
+    {
+        FileModifyType[] options = { FileModifyType.ACCEPT, FileModifyType.ACCEPT_ALL, FileModifyType.DELETE, FileModifyType.INSERT, FileModifyType.REPLACE };
+        int selection = JOptionPane.showOptionDialog(this, "Select mode", "Mode Selection", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, FileModeType.READ );
+        
+        // User pressed X (close) button instead of READ or WRITE or MODIFY
+        if( selection < 0 )
+        {
+           System.exit(-5);
+        }
+        
+        return options[selection];
+    }
+    
+    
+    /**
+     * Method: getReadWriteMode<br/>
+     * Ask the user if they want to READ or WRITE or MODIFY to a given file
+     *
+     * @return the selection of READ or WRITE or MODIFY or exit if the X is pressed on the window
+     */
+    public FileModeType getReadWriteModifyMode()
+    {
+        FileModeType[] options = { FileModeType.MODIFY, FileModeType.READ, FileModeType.WRITE};
+        int selection = JOptionPane.showOptionDialog(this, "Select mode", "Mode Selection", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, FileModeType.READ );
+        
+        // User pressed X button instead of READ or WRITE
+        if( selection < 0 )
+        {
+           System.exit(-5);
+        }
+        
+        return options[selection];
+    }
+    
+    
+    /**
+     * Method: getSingleNumberInput<br/>
+     * Ask the user for input of a Double (Real number) and enforce entry
+     *
+     * @param identifier - the special identifier to display to the user
+     * @return the Double/Real number value the user entered
+     */
+    private Double getSingleNumberInput( int rowId, int colId )
+    {
+    	String prompt = "Input number";
+    	
+    	if( rowId >= 1 && colId >= 1 )
+    	{
+    		prompt += " for row " + rowId + ", column " + colId;
+    	}
+    	
+       String input = JOptionPane.showInputDialog( prompt );
+       Double result = null;
+       
+       if( input == null )
+       {
+           // User pressed cancel button, just exit for now
+           System.exit(-6);
+       }
+       
+       try
+       {
+           result = Double.parseDouble( input );
+       }
+       catch( NumberFormatException nfe )
+       {
+           // User didn't enter a double (Real number)
+           displayWarning( "You didn't enter a Real number! Try again.", "Number input error" );
+           return getSingleNumberInput( rowId, colId );
+       }
+       
+       return result;
+    }
+    
+    
+    /**
+     * Method: getNumbersInput<br/>
+     * Ask the user for a given set of numbers as determined by quantityOfNumbersToInput
+     *
+     * @param quantityOfNumbersToInput - How many Real numbers to ask the user for
+     * @return an ArrayList<Double> of numbers the user input
+     */
+    public ArrayList<String> getNumbersInput( int rowId, Integer quantityOfNumbersToInput )
+    {
+        ArrayList<String> numbersInputted = new ArrayList<String>();
+        
+        for( int i=0; i<quantityOfNumbersToInput; i++ )
+        {
+            String input = getSingleNumberInput( rowId, i+1 ) + "";
+            numbersInputted.add( input );
+        }
+        
+        return numbersInputted;
+    }
+    
+    
+    /**
+     * Method: getQuantityOfNumbers<br/>
+     * Ask the user for how many numbers they are going to input
+     *
+     * @return the quantity of numbers the user is planning to input
+     */
+    public Integer getQuantityOfNumbers()
+    {
+        Integer quantity = null;
+        
+        String input = JOptionPane.showInputDialog( "How many rows of numbers are you going to record?\nEach row will have K=" + Main.ENTRIES_PER_ROW +" items." );
+        
+        if( input == null )
+        {
+            // User pressed cancel button, just exit for now
+            System.exit(-6);
+        }
+        
+        try
+        {
+            quantity = Integer.parseInt( input );
+        }
+        catch( NumberFormatException nfe )
+        {
+            // User didn't enter a number (jerk!), return them to the input screen.
+            displayWarning( "You didn't enter in a number! Try again.", "Number input error" );
+            return getQuantityOfNumbers();
+        }
+        
+        return quantity;
+    }
+    
+    
+    /**
+     * Method: getFileLocation<br/>
+     * Ask the user where to READ/WRITE the file from
+     *
+     * @return the absolute path of the file the user wants to READ/WRITE from
+     */
+    public String getFileLocation(String title, String filtername, String filterfile)
+    {
+        String location = null;
+
+        JFileChooser chooser = new JFileChooser();
+        
+        // Filter out everything but .txt files by default.
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(filtername, filterfile);
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showDialog( this, title );
+        
+        if (returnVal == JFileChooser.APPROVE_OPTION)
+        {
+            System.out.println("You chose to open/create this file: " + chooser.getSelectedFile().getName());
+        }
+        else
+        {
+            System.exit(-2);
+        }
+
+        location = chooser.getSelectedFile().getAbsolutePath();
+        ArrayList<Character> invalidFilenames = new ArrayList<Character>();
+        invalidFilenames.add('\\');
+        invalidFilenames.add('/');
+        invalidFilenames.add(':');
+        invalidFilenames.add('*');
+        invalidFilenames.add('?');
+        invalidFilenames.add('\"');
+        invalidFilenames.add('<');
+        invalidFilenames.add('>');
+        invalidFilenames.add('|');
+        String filename = chooser.getSelectedFile().getName();
+        
+        char[] filenameChars = filename.toCharArray();
+        for( int i=0; i<filenameChars.length; i++ )
+        {
+            if( invalidFilenames.contains(filenameChars[i]) )
+            {
+                location = null;
+            }
+        }
+        
+        if( location == null || location.equals("") )
+        {
+            displayWarning( "You didn't enter a valid file name!", "Invalid file name" );
+            return getFileLocation(title, filtername, filterfile);
+        }
+        
+        return location;
+    }
+
+  
+    /**
+     * Method: initializeAndDisplayWithPrompt<br/>
+     * Initialize the display as normal, but for each line in the, prompt the user
+     * for input.
+     * Once the user is done, allow them to save this new output as a file.
+     *
+     * @param fileContents - The contents of the file in an ArrayList<String>
+     */
+	public void initializeAndDisplayWithPrompt( ArrayList<ArrayList<String>> fileContents )
+    {
+    	initializeAndDisplay();
+    	
+    	FileModifyType selection = null;
+    	
+    	int counter = 0;
+    	for( ArrayList<String> arr : fileContents )
+    	{
+    	        displayArrayListSingleRow( arr );
+                
+                if( selection != FileModifyType.ACCEPT_ALL )
+                {
+                    // This is a blocking call - nothing will happen until user selection is made
+                    selection = getModifyAction();
+                }
+
+                
+                // Handle the different selections from the user
+                if( selection == FileModifyType.ACCEPT )
+                {
+                    // Don't need to do anything
+                }
+                else if( selection == FileModifyType.ACCEPT_ALL )
+                {
+                    // Don't need to do anything
+                    // Bypass asking for more input print out everything else
+                }
+                else if( selection == FileModifyType.DELETE )
+                {
+                    removeSingleRow( arr );
+                }
+                else if( selection == FileModifyType.INSERT )
+                {
+                    ArrayList<String> input = getNumbersInput( counter+1, Main.ENTRIES_PER_ROW );
+                    displayArrayListSingleRow( input );
+                }
+                else if( selection == FileModifyType.REPLACE )
+                {
+                    ArrayList<String> input = getNumbersInput( counter+1, Main.ENTRIES_PER_ROW );
+                    removeSingleRow( arr );
+                    displayArrayListSingleRow( input );
+                }
+                else
+                {
+                    System.err.println("Unsupported file modify type!");
+                }
+            counter++;
+    	} // end for
+    	
+    	
+    	
+    }
+    
+	
+	/**
+     * Method: displaySaveAsDialog<br/>
+     * Allow the user to save the file either as a new file or as the existing one.
+     *
+     */
+    private void displaySaveAsDialog()
+    {
+    	String saveAsLocation = getFileLocation("Save file as", ".txt files", "txt" );
+    	try {
+			MyFileWriter fileWriter = new MyFileWriter( saveAsLocation );
+			System.out.println("Saving these contents: " + getJListAsArrayList() );
+			fileWriter.setFileContentsWithList( getJListAsArrayList() );
+			System.out.println("Saved @ " + saveAsLocation );
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+
+    
+    /**
+     * Method: initializeAndDisplay<br/>
+     * Initialize GUI components
+     *
+     */
+    public void initializeAndDisplay()
+    {
+        this.setSize( DEFAULT_WINDOW_SIZE );
+        this.setTitle( WINDOW_TITLE );
+        this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        JPanel panel = new JPanel( new BorderLayout(3, 0) );
+        JPanel fillerPanelEast = new JPanel();
+        JPanel fillerPanelWest = new JPanel();
+        JPanel fillerPanelSouth = new JPanel();
+        JPanel myLabelPanel = new JPanel();
+        
+        // Setup the File > Save As.. functionality for Assignment 2B
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        
+        JMenuItem menuItem = new JMenuItem();
+        menuItem.setText("Save As...");
+        menuItem.addActionListener( new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				displaySaveAsDialog();
+			}
+        	
+        });
+        menu.add( menuItem );
+        
+        menuBar.add( menu );
+        setJMenuBar( menuBar );
+        
+        // Setup all of the panels around the JList to be DARK_GRAY for consistency (and good looks).
+        fillerPanelEast.setBackground( Color.DARK_GRAY );
+        fillerPanelWest.setBackground( Color.DARK_GRAY );
+        fillerPanelSouth.setBackground( Color.DARK_GRAY );
+        myLabelPanel.setBackground( Color.DARK_GRAY );
+        
+        
+        // This label will be just above the JList that holds the file data
+        JLabel label = new JLabel("File Contents");
+        label.setFont( new Font("Arial", Font.BOLD, 16) );
+        label.setForeground( Color.WHITE );
+        myLabelPanel.add( label );
+        
+        
+        panel.add( fillerPanelEast, BorderLayout.EAST );
+        panel.add( fillerPanelWest, BorderLayout.WEST );
+        panel.add( fillerPanelSouth, BorderLayout.SOUTH );
+        
+        panel.add( myLabelPanel, BorderLayout.NORTH );
+        panel.add( getJListPane(), BorderLayout.CENTER );
+        
+        // Add to main frame
+        add( panel );
+        
+        // Set the location of the panel to the center of the user's screen
+        this.setLocationRelativeTo( getRootPane() );
+        
+        this.setVisible(true);
+    }
+} // end class
